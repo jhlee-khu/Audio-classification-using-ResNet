f074545c83935fccac313a79cd2c278349ccb66f
Lee.J.H <karna@khu.ac.kr>
2024-10-01 20:48:24 +0900

    <major changes> 1. deviding audio files to multiple 960ms patches. 2. changing STFT window size and hop size to make spectrogram has 64 by 64 size. 3. changing image directory and annotation files and etc to make model easier to train/ validate/ test

--- convert_wav_to_spectrogram.py
+++ convert_wav_to_spectrogram.py
@@ -1,6 +1,5 @@
 # convert audio to spectrogram(img file)
 # place this py file into the ./Urbansound8K directory
-# urbansound8k.csv file must also be in the same directory (./Urbansound8K )
 
 import csv
 import librosa
@@ -8,30 +7,60 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-def spectrogram_image(filename,fold,filepath):
+def split_y_n_pad(y,patchsize):
+    # split y to multiple array (patches) which length == patchsize
+    # if patch size is smaller than var(patchsize) => zero pad
+    num_patches = int(np.ceil(len(y) / patchsize))
+    padded = np.pad (y, (0, patchsize * num_patches - len(y)),'constant')
+    patches = np.reshape (padded, (-1,patchsize))
+
+    return patches
+
+def spectrogram_image(filename,fold,filepath,class_num):
     y, sr = librosa.load(filepath)
 
-    # right zero padded if audio length is less than 5 s.
-    # As urbansound 8k dataset audios are 0~4 s
-    # all spectrograms has been zero padded.
-    y = np.pad(y,(0,sr * 5 - len(y)),'constant',constant_values = 0) 
+    patchsize = int(sr * 640/1000)
+    patches = split_y_n_pad(y,patchsize)
+    patch_idx = len(patches)
 
-    hop = int(np.round(sr/32)) 
-    window_size = 2 * hop # window size will be 1/16 s, and hop 1/32 s
+    hop = int(np.round(patchsize / 64)) 
+    window_size = int(np.round(2.5 * hop)) # window size will be 25 ms, and hop 10 ms
 
-    stft = librosa.stft(y, n_fft = window_size, hop_length = hop)
-    mel = librosa.filters.mel(sr = sr, n_fft = window_size, n_mels = 128)
-    power = np.abs(stft) ** 2
-    mel_spec = np.dot(mel, power)
+    annot_file_path = './annotation.csv'
+    f = open (annot_file_path,'a',newline = '')
+    wr = csv.writer(f)
+
+    for i in range(0,patch_idx):
+        stft = librosa.stft(patches[i], n_fft = window_size, hop_length = hop)
+        mel = librosa.filters.mel(sr = sr, n_fft = window_size, n_mels = 64)
+        power = np.abs(stft) ** 2
+        mel_spec = np.dot(mel, power)
+        fig = plt.figure(figsize = (64/77,64/77))
+        librosa.display.specshow(mel_spec, sr=sr, hop_length = hop)
+
+        # fold 1~7 => training set
+        # fold 8,9 => validation set
+        # fold 10 => testset
+        if (fold  <= 7):
+            new_filepath = './training/' + str(i) + '-' + filename+'.png'
+        elif (fold == 8 or fold == 9):
+            new_filepath = './validation/' + str(i) + '-' + filename+'.png'
+        else:
+            new_filepath = './test/' + str(i) + '-' + filename+'.png'
+        
+        fig.savefig(fname = new_filepath, bbox_inches = 'tight', pad_inches = 0)
+
+        #write annotation file
+        row = [str(i) + '-' + filename+'.png',class_num]
+        wr.writerow(row)
+        
+        plt.close()
+    f.close()
     
-    fig = plt.figure(figsize = (4,4))
-    librosa.display.specshow(mel_spec, sr=sr, hop_length = hop)
-    new_filepath = './convfold'+fold+'/'+filename+'.png'
-    fig.savefig(fname = new_filepath, bbox_inches = 'tight', pad_inches = 0)
-    plt.close()
-    
+
+
 # main loop
-metadata = open('UrbanSound8K.csv','r')
+metadata = open('./metadata/UrbanSound8K.csv','r')
 line = csv.reader(metadata)
 
 itr = -1
@@ -41,5 +70,12 @@ for ln in line:
     if itr == 0: continue
     filename = ln[0]
     fold = ln[5]
+    class_num = ln[6]
     filepath = './audio/fold' + fold + '/' + filename
-    spectrogram_image(filename,fold,filepath)
+    spectrogram_image(filename,int(fold),filepath,class_num)
+
+''' 
+#test 
+    if itr == 5:
+        break
+'''
