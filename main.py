7e53edc417708cd85db1aa882333c8d87f82ffd1
jhlee-khu <a3lements@gmail.com>
2025-02-18 14:13:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,18 @@ from torchvision.io import read_image
 from torch.utils.data import DataLoader
 from PIL import Image
 
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["TORCH_USE_CUDA_DSA"] = '1'
 
 import sys
-sys.path.append('/data/$USER/repos/khu_cse_capstone13_24_2') # your path
+sys.path.append('/data/karna/repos/khu_cse_capstone13_24_2') # your path
 from resnet import *
+from TF_ResNet import *
 
 # cell 2
 parser = argparse.ArgumentParser(description='PyTorch Spectrogram training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--lr', default=0.0001, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args(args = [])
@@ -48,66 +52,95 @@ class audio_spectrogram(Dataset):
         return len(self.img_labels)
     def __getitem__(self, idx):
         spect_path = self.img_dir + '/' +self.img_labels.iloc[idx, 0]
-        spect = img = Image.open(spect_path).convert('RGB')
-        label = self.img_labels.iloc[idx, 1]
+        spect = Image.open(spect_path).convert('RGB')
+        
+        pth = self.img_labels.iloc[idx, 0].split('/')
 
-        if self.transform: spect = self.transform(spect)
+        H_spect_path = self.img_dir + '/' + pth[0] + '/H_' + pth[1] 
+        H_spect = Image.open(H_spect_path).convert('RGB')
+
+        P_spect_path = self.img_dir + '/' + pth[0] + '/P_' + pth[1] 
+        P_spect = Image.open(P_spect_path).convert('RGB')
+
+        if self.transform: 
+            spect = self.transform(spect)
+            H_spect = self.transform(H_spect)
+            P_spect = self.transform(P_spect)
+        label = int(self.img_labels.iloc[idx, 1])
         if self.target_transform: label = self.target_transform(label)
 
-        return spect,label
+        return torch.stack([spect, H_spect, P_spect],dim = 0) ,label
 
 # cell 4
 
 # label map and allocate train and test set pathes
 # initialize
 
-labels = ("air_conditioner","car_horn","children_playing","dog_bark","drilling","engine_idling","gun_shot","jackhammer","siren","street_music")
+#fold_num
+foldnum = '3_'
 
-trainannot_path = '/data/$USER/repos/khu_cse_capstone13_24_2/us8k_train.csv' # your path
-validationannot_path = '/data/$USER/repos/khu_cse_capstone13_24_2/us8k_valid.csv' # your path
+trainannot_path = '/local_datasets/unified_spect_folds/' + foldnum + 'train_annot.csv'
+validationannot_path = '/local_datasets/unified_spect_folds/' + foldnum + 'valid_annot.csv' 
 
-train_spect_path = '/local_datasets/us8k_train' # your path
-validation_spect_path = '/local_datasets/us8k_valid' # your path
+train_spect_path = '/local_datasets/unified_spect_folds/' 
+validation_spect_path = '/local_datasets/unified_spect_folds/'
 
 trainset = audio_spectrogram(trainannot_path, train_spect_path,transform = transforms.ToTensor())
 validationset = audio_spectrogram(validationannot_path, validation_spect_path,transform = transforms.ToTensor())
+
 # cell 5
 #data loader
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(validationset, batch_size=32, shuffle=False, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(validationset, batch_size=64, shuffle=False, num_workers=2)
 
-net = ResNet18()
+
+model_name = 'ResNet34_fold3'
+# net = TF_ResNet34()
+net = ResNet34(2)
 net = net.to(device)
+
+train_acc = np.array([]).astype(np.float32)
+test_acc = np.array([]).astype(np.float32)
+train_loss_arr = np.array([])
+classacc = np.zeros(11)
+test_loss_arr = np.array([])
+
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
 if args.resume:
-#if True:
+# if True:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('/data/$USER/repos/khu_cse_capstone13_24_2/checkpoint/ckpt.pth')
+    checkpoint = torch.load('/data/karna/repos/khu_cse_capstone13_24_2/checkpoint/' + model_name +'_ckpt.pth')
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
 
+    train_acc = np.load('/data/karna/repos/khu_cse_capstone13_24_2/accdata/trainacc_'+ model_name + '.npy')
+    test_acc = np.load('/data/karna/repos/khu_cse_capstone13_24_2/accdata/testacc_' + model_name + '.npy')
+    classacc = np.load('/data/karna/repos/khu_cse_capstone13_24_2/accdata/classacc_'+ model_name  + '.npy')
+    train_loss_arr = np.load('/data/karna/repos/khu_cse_capstone13_24_2/accdata/train_loss_'+ model_name  + '.npy')
+    test_loss_arr = np.load('/data/karna/repos/khu_cse_capstone13_24_2/accdata/test_loss_' + model_name + '.npy')
+
+
 # cell 6
 #model setting
-
-train_acc = np.array([]).astype(np.float32)
-test_acc = np.array([]).astype(np.float32)
 
 from torch.optim.lr_scheduler import StepLR
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), weight_decay = 0.0002)
-scheduler = StepLR(optimizer, step_size=10, gamma=0.6)
+optimizer = optim.Adam(net.parameters(),lr = 0.001)
+scheduler = StepLR(optimizer, step_size=50, gamma=0.1)
+
+
 
 # cell 7
 # Training
 def train(epoch):
-    global train_acc
+    global train_acc,train_loss_arr
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
@@ -116,8 +149,10 @@ def train(epoch):
 
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
+        
         optimizer.zero_grad()
         outputs = net(inputs)
+
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
@@ -128,16 +163,24 @@ def train(epoch):
         correct += predicted.eq(targets).sum().item()
         if (batch_idx == len(trainloader) - 1):
             train_acc = np.append(train_acc,100.*correct/total)
+            train_loss_arr = np.append(train_loss_arr, train_loss/(batch_idx+1))
             print('train %d epoch, Loss: %.3f | Acc: %.3f%% (%d/%d)' % (epoch, train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
+best_class_predicted = np.zeros((11,11))
 
 # Test & validation
 def test(epoch):
-    global best_acc, test_acc
+    class_predicted = np.zeros((11,11))
+    global best_acc, test_acc,string_sec, classacc, test_loss_arr, model_name, best_class_predicted
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
+    num_classes = 11
+
+    class_correct = np.zeros(num_classes)
+    class_total = np.zeros(num_classes)
+
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
@@ -149,9 +192,19 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
+            for i in range(len(targets)):
+                label = targets[i].item()
+                class_total[label] += 1
+                if predicted[i] == label:
+                    class_correct[label] += 1
+                class_predicted[label][predicted[i]] += 1
+
             if (batch_idx == len(testloader) - 1):
-                test_acc = np.append(test_acc,100.*correct/total)
+                test_acc = np.append(test_acc, 100.*correct/total)
+                test_loss_arr = np.append(test_loss_arr, test_loss/(batch_idx+1))
                 print('test %d epoch, Loss: %.3f | Acc: %.3f%% (%d/%d)' % (epoch, test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+
 
     # Save checkpoint.
     acc = 100.*correct/total
@@ -162,15 +215,24 @@ def test(epoch):
             'acc': acc,
             'epoch': epoch,
         }
+
+        for i in range(num_classes):
+            classacc[i] = 100. * class_correct[i] / class_total[i]
+        best_class_predicted = class_predicted.copy()
+
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
-        torch.save(state, '/data/$USER/repos/khu_cse_capstone13_24_2/checkpoint/ckpt.pth')
+        torch.save(state, '/data/karna/repos/khu_cse_capstone13_24_2/checkpoint/' + model_name +'_ckpt.pth')
         best_acc = acc
+
 
 for epoch in range(start_epoch, start_epoch + 80):
     train(epoch)
     test(epoch)
-    scheduler.step()
 
-np.save('/data/$USER/repos/khu_cse_capstone13_24_2/trainacc.npy', train_acc)
-np.save('/data/$USER/repos/khu_cse_capstone13_24_2/testacc.npy', test_acc)
+np.save('/data/karna/repos/khu_cse_capstone13_24_2/accdata/trainacc_'+ model_name  + '.npy', train_acc)
+np.save('/data/karna/repos/khu_cse_capstone13_24_2/accdata/testacc_' + model_name + '.npy', test_acc)
+
+np.save('/data/karna/repos/khu_cse_capstone13_24_2/accdata/train_loss_' + model_name + '.npy', train_loss_arr)
+np.save('/data/karna/repos/khu_cse_capstone13_24_2/accdata/test_loss_' + model_name + '.npy', test_loss_arr)
+np.save('/data/karna/repos/khu_cse_capstone13_24_2/accdata/class_predicted_' + model_name + '.npy', best_class_predicted)
